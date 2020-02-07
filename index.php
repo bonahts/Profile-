5d018f250461f816e2605274dd78f7b8348917b6
bonahts <johndoe@example.com>
2020-02-07 11:29:11 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My personal info</title>
+    <link rel="stylesheet" href="style/style.css" type="text/css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,700">
+</head>
+
+<body>
+
+
+<div class="top-bar col-12">
+<b style="font: italic; color: black ">
+Welcome to My Profile Information
+</b>
+</div>
+
+<div class="jumbotron">
+    <img src="image/background.jpg" style="width: 100%; filter: blur(1px);" >
+    <div class="bg-text"> 
+
+        <img src="image/profilepicture.jpg" alt="avatar" class="profile-picture">
+        <h2 style="margin-left: 30px; color:black;">Hi! I am Bona </h2>
+        <p style="width:220px; display:inline; color: white;">I am beginner web developer. This is my first project.</p>
+    </div>
+</div>
+
+
+<div class="row">
+    <div class="col-3"></div>
+    <div class="col-3 profile-desc">
+        <h3 style="color: Blue;">My Story</h3>
+        <p style="color: green;">My name is Bona Joshua Hutasoit. i came from Medan, North Sumatra and studied at UPN Veteran Yogyakarta.</p>
+    </div>
+    <div class="col-3 profile-desc">
+        <h3>My Biodata</h3>
+        <table>
+            <tr>
+                <td><b>Name :</b></td>
+                <td>Bona Joshua Hutasoit</td>
+            </tr>
+            <tr>
+                <td><b>Status :</b></td>
+                <td>Student</td>
+            </tr>
+            <tr>
+                <td><b>Gender :</b></td>
+                <td>Male</td>
+            </tr>
+        </table>
+    </div>
+
+</div>
+
+<div style="background-color: rgb(62,254,255); padding:5px;">
+        <p style="margin-left: 30px; color: black">Copyright &copy; 2018</p>
+        
+    </div> 
+
+
+
+
+    
+</body>
+</html>
